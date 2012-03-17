00bd86611463f73ebd3b85476d7b8fa79df6e4e9
Johan Bruning <johan.h.bruning@gmail.com>
2012-03-17 17:26:17 +0100

    Show recently viewed products inside homepage_sidebar_navigation on products page & home

--- app/overrides/add_recently_viewed_products.rb
+++ app/overrides/add_recently_viewed_products.rb
@@ -1,9 +1,8 @@
-Deface::Override.new(:virtual_path => 'spree/shared/_products',
+Deface::Override.new(:virtual_path => 'spree/products/index',
                      :name => 'add_recently_viewed_products_to_products_index',
-                     :insert_after => "#products[data-hook], [data-hook='products']",
+                     :insert_top => "[data-hook='homepage_sidebar_navigation']",
                      :partial => 'spree/shared/recently_viewed_products')
-
-Deface::Override.new(:virtual_path => 'spree/products/show',
-                     :name => 'add_recently_viewed_products_to_products_show',
-                     :insert_after => "#product_description[data-hook], [data-hook='product_description']",
+Deface::Override.new(:virtual_path => 'spree/home/index',
+                     :name => 'add_recently_viewed_products_to_home_page',
+                     :insert_top => "[data-hook='homepage_sidebar_navigation']",
                      :partial => 'spree/shared/recently_viewed_products')
